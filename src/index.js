7ee458db35b1b17a05a1a158eb731753dede4ac1
Sukanya Sardar <sukanyasardar44@gmail.com>
2021-03-31 01:47:24 +0530

    index5 done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}></StateProvider>
     <App />
+    <StateProvider/>
   </React.StrictMode>,
   document.getElementById('root')
 );
