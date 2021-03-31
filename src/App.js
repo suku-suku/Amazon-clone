738042221014c2facc652c05fa8ea7fc638f646a
Sukanya Sardar <sukanyasardar44@gmail.com>
2021-03-31 10:11:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import { auth } from "./firebase";
-import Home from "./Home"
-import Header from "./Header"
-import Login from "./Login"
+import Home from "./Home";
+import Header from "./Header";
+import Login from "./Login";
 import { useStateValue } from "./StateProvider";
+import Product from "./Product";
 
 
-useEffect(() => {
-  // will only run once when the app component loads...
-
-  auth.onAuthStateChanged((authUser) => {
-    console.log("THE USER IS >>> ", authUser);
-
-    if (authUser) {
-      // the user just logged in / the user was logged in
-
-      dispatch({
-        type: "SET_USER",
-        user: authUser,
-      });
-    } else {
-      // the user is logged out
-      dispatch({
-        type: "SET_USER",
-        user: null,
-      });
-    }
-  });
-}, []);
 
 
-function App() {
 
-  const [{}, dispatch] = useStateValue();
   
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="app">
-      <Router>
+    <Router>
+      <div className="app">
         <Switch>
-        <Route path="/Login">
-  <Login />
-</Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
           <Route path="/">
-            <Header/>
+            <Header />
             <Home />
           </Route>
         </Switch>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-/*
+
 import firebase from "firebase";
 
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3P-qr7Mrlg74BQymT7bykV2HYwbU8Gk8",
-  authDomain: "amz-clone-medium.firebaseapp.com",
-  databaseURL: "https://amz-clone-medium.firebaseio.com",
-  projectId: "amz-clone-medium",
-  storageBucket: "amz-clone-medium.appspot.com",
-  messagingSenderId: "769450879220",
-  appId: "1:769450879220:web:cbff61e0ee041531e456a1",
-  measurementId: "G-CZDBWXMWF7",
+  apiKey: "AIzaSyBwh4PxitUOODXgK_JuXRDSGWxvFvg6taM",
+  authDomain: "clone-d9603.firebaseapp.com",
+  projectId: "clone-d9603",
+  storageBucket: "clone-d9603.appspot.com",
+  messagingSenderId: "1023504600924",
+  appId: "1:1023504600924:web:60ca0521b2a866086e3b99",
+  measurementId: "G-431Y474QHP"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -19,4 +18,3 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
 export { db, auth };
-*/
