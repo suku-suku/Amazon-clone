71dbcd9830e7d66816c0d6e0fec5a5a278424370
Sukanya Sardar <sukanyasardar44@gmail.com>
2021-03-31 07:05:55 +0530

    with warnings

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,6 @@ import "./Product.css";
 import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
-
   const [{ basket }, dispatch] = useStateValue();
 
   const addToBasket = () => {
@@ -19,13 +18,13 @@ function Product({ id, title, image, price, rating }) {
       },
     });
   };
- 
+
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <small>Rs</small>
+          <small>$</small>
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
