a551a1a99f32b8bd62689d7ac6cc975ce37567f1
Sukanya Sardar <sukanyasardar44@gmail.com>
2021-03-31 07:57:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import { auth } from "./firebase";
 import Home from "./Home"
 import Header from "./Header"
+import Login from "./Login"
+import { useStateValue } from "./StateProvider";
+
+
+useEffect(() => {
+  // will only run once when the app component loads...
+
+  auth.onAuthStateChanged((authUser) => {
+    console.log("THE USER IS >>> ", authUser);
+
+    if (authUser) {
+      // the user just logged in / the user was logged in
+
+      dispatch({
+        type: "SET_USER",
+        user: authUser,
+      });
+    } else {
+      // the user is logged out
+      dispatch({
+        type: "SET_USER",
+        user: null,
+      });
+    }
+  });
+}, []);
+
 
 function App() {
+
+  const [{}, dispatch] = useStateValue();
+  
   return (
     <div className="app">
       <Router>
         <Switch>
+        <Route path="/Login">
+  <Login />
+</Route>
           <Route path="/">
             <Header/>
             <Home />
