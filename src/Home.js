5f5a10b991da003a3020fedbe92a33ad85f8b3f0
Sukanya Sardar <sukanyasardar44@gmail.com>
2021-04-10 06:11:33 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -8,60 +8,95 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/in_prime_pod_gw_adapts_acquisition_1500x600._CB405871129_.jpg"
+         // src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9jfcP9OLMS6QDf5WUl5hm557o-h5GSi4-FA&usqp=CAU"
           alt=""
         />
 
         <div className="home__row">
           <Product
             id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            title="Tokyo Talkies"
+           // Women Black & White Checked Fit and Flare Dress"
             price={11.96}
             rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2019/7/25/2da82277-23cf-474b-8815-f6cc8e884b971564055901027-1.jpg"
           />
           <Product
             id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            title="Kenwood kMix "//Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
             price={239.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/10856160/2019/11/5/f0172470-408a-4b8b-a1e5-045bd98a5f8b1572938035104-SASSAFRAS-Women-Dresses-9581572938032856-1.jpg"
           />
-        </div>
+        
 
-        <div className="home__row">
+      
           <Product
             id="4903850"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            title="Printed Straight Kurta"
             price={199.99}
             rating={3}
-            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/11056154/2019/12/5/30b0017d-7e72-4d40-9633-ef78d01719741575541717470-AHIKA-Women-Black--Green-Printed-Straight-Kurta-990157554171-1.jpg"
           />
           <Product
             id="23445930"
-            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            title="Angrakha Kurta"
             price={98.99}
             rating={5}
-            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/13647622/2021/3/4/1fa584db-2f9e-4a0f-8ee3-f649a92eb3de1614860439664-Varanga-Women-Pink--White-Leheriya-Printed-Angrakha-Kurta-Wi-1.jpg"
           />
           <Product
             id="3254354345"
-            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            title="Floral Dress"
             price={598.99}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/9/16/25f5b6a7-ce25-4e66-8a61-3aa2037b6c741600207614629-1.jpg"
           />
         </div>
 
+        
         <div className="home__row">
           <Product
-            id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
+            id="12321341"
+            title="Tokyo Talkies"
+           // Women Black & White Checked Fit and Flare Dress"
+            price={11.96}
+            rating={5}
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2020/10/15/0ed2afd4-a106-4972-9499-111c95fcc15c1602713909748-1.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Kenwood kMix "//Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={239.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/10968582/2020/2/18/19c50d33-f838-453e-8ccc-838df5ca00b41582017499216-Roadster-Women-Tshirts-9451582017497392-1.jpg"
+          />
+        
+
+      
+          <Product
+            id="4903850"
+            title="Printed Straight Kurta"
+            price={199.99}
+            rating={3}
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/productimage/2021/1/27/c3f87e02-333b-4008-aa5c-3bb5d3fc658a1611743836816-1.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Angrakha Kurta"
+            price={98.99}
+            rating={5}
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/2477343/2018/2/22/11519301743689-Libas-Women-Green-Woven-Design-Pathani-Kurta-4531519301743438-1.jpg"
+          />
+          <Product
+            id="3254354345"
+            title="Floral Dress"
+            price={598.99}
+            rating={4}
+            image="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/11364418/2020/1/28/8e31363f-8ac5-4669-9aa8-114d502d7be61580207577109-SASSAFRAS-Women-Maroon-Solid-Tiered-Maxi-Dress-3781580207574-1.jpg"
           />
         </div>
+
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -20,25 +20,26 @@ function Product({ id, title, image, price, rating }) {
   };
 
   return (
-    <div className="product">
+   
+   // <img src={image} alt="" />
+   <div className="product">
+
+<img src={image} alt="" />
+
+
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
           <small>Rs</small>
           <strong>{price}</strong>
         </p>
-        <div className="product__rating">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <p>🌟</p>
-            ))}
-        </div>
+       
+        
       </div>
 
-      <img src={image} alt="" />
+      
 
-      <button onClick={addToBasket}>Add to Basket</button>
+      <button onClick={addToBasket}>WishList</button>
     </div>
   );
 }
